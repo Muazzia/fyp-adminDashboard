263c23c09dcab2c78647e0429939b3667d297149
Muaz <muazzia17@gmail.com>
2024-10-30 22:59:04 +0500

    add logout button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { FaPowerOff } from "react-icons/fa6";
+import { LS_TOKEN } from "./../../constants/index";
 const Header = () => {
+  const nav = useNavigate();
+
+  const handleClick = () => {
+    localStorage.setItem(LS_TOKEN, "");
+    nav("/signin");
+  };
+
   return (
-    <div className="flex px-4 py-4 bg-main text-white">
+    <div className="flex items-center justify-between px-4 py-4 bg-main text-white">
       <Link to={"/"} className="font-bold cursor-pointer">
         Skin Guardian
       </Link>
+      <FaPowerOff onClick={handleClick} className="hover:cursor-pointer" />
     </div>
   );
 };
